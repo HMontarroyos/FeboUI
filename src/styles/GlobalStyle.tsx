1982aee0d653ad8b0e566187e897fb851c1d01f9
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2023-08-10 15:26:39 -0300

    feat: add styles global in project

---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    border: 0;
+    padding: 0;
+    margin: 0;
+    font-family: 'Rasa', serif;
+  }
+
+`;
