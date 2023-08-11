b56d80b7148ee78981c3799b5aaeb9132735ca6b
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2023-08-10 22:45:36 -0300

    dix: adjust npm ignore and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,12 @@ module.exports = {
   entry: "./src/components/index.tsx",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "febo-ui.tsx",
+    filename: "febo-ui.js", 
     library: "febo-ui",
     libraryTarget: "umd",
     umdNamedDefine: true,
     globalObject: "this",
+    libraryExport: "default", 
   },
   module: {
     rules: [
@@ -34,48 +35,5 @@ module.exports = {
   },
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
-    alias: {
-      "@components": path.resolve(__dirname, "src/components"),
-    },
   },
 };
-
-/* const path = require('path');
-
-module.exports = {
-  entry: './src/components/index.tsx',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js',
-    library: '@hmontarroyos/apolo-ui',
-    libraryTarget: 'umd',
-    umdNamedDefine: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx|ts|tsx)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              outputPath: 'images',
-            },
-          },
-        ],
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
-  },
-  externals: {
-    react: 'react',
-    'react-dom': 'react-dom',
-  },
-}; */
