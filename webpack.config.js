e18fcae5179d7c923ebf928357f9a5759172425e
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2023-08-10 22:27:20 -0300

    fix: adjust webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,46 @@
-const path = require('path');
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/components/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "febo-ui.tsx",
+    library: "febo-ui",
+    libraryTarget: "umd",
+    umdNamedDefine: true,
+    globalObject: "this",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "images",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+    alias: {
+      "@components": path.resolve(__dirname, "src/components"),
+    },
+  },
+};
+
+/* const path = require('path');
 
 module.exports = {
   entry: './src/components/index.tsx',
@@ -36,4 +78,4 @@ module.exports = {
     react: 'react',
     'react-dom': 'react-dom',
   },
-};
+}; */
