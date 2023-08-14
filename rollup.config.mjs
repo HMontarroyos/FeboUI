30faf987794567fd761c2e950c69a91693c1d681
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2023-08-14 16:35:14 -0300

    fix: rollup replace processe.env in {}

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,7 +2,8 @@ import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import image from 'rollup-plugin-image-files';
-import babel from '@rollup/plugin-babel'; 
+import babel from '@rollup/plugin-babel';
+import replace from '@rollup/plugin-replace'; 
 
 export default {
   input: './src/components/index.tsx', 
@@ -22,6 +23,9 @@ export default {
     babel({
       presets: ["@babel/preset-react", "@babel/preset-env"],
       plugins: ["babel-plugin-styled-components"]
+    }),
+    replace({
+      'process.env': '{}',
     }),
     image(),
   ],
