e34ad0dacc250f57f6285191078fe83aedb4bd5a
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2023-08-25 16:16:41 -0300

    feat: add component toggle in home app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React from "react";
+import { Toggle } from "./components";
 
 function App(): JSX.Element {
   return (
+    <>
       <h1>Febos UI</h1>
+      <Toggle onChange={() => console.log("TESTE")}  defaultOn={false}/>
+    </>
   );
 }
 
