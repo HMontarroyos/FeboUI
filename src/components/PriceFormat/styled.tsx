45ec4d4d87119563895e3220b775d4fbebce99cb
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2023-10-26 10:52:30 -0300

    feat: add news props in component Price Format

--- src/components/PriceFormat/styled.tsx
+++ src/components/PriceFormat/styled.tsx
@@ -3,17 +3,18 @@ import styled from "styled-components";
 interface Props {
   color?: string;
   bold?: boolean;
+  fontSize?: number;
+  strike?: boolean;
 }
 
-export const Container = styled.div`
-
-`;
+export const Container = styled.div``;
 
 export const TextPrice = styled.p<Partial<Props>>`
   padding: 0;
   margin: 0;
-  font-size: 12px;
+  font-size: ${(props) => `${props.fontSize}px`};
   font-weight: ${(props) => props.bold && "bold"};
-  color: ${(props) => props.color};
+  color: ${(props) => (props.strike ? "grey" : props.color)}; 
   text-align: center;
+  text-decoration: ${(props) => props.strike && "line-through"}; 
 `;
