879fe36cd3c27ca7488bde1a7a9632058870cde4
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2023-10-26 06:50:01 -0300

    feat: add component PriceFormat

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -5,6 +5,17 @@ import Loading from "./Loading";
 import TouchOverlay from "./TouchOverlay";
 import Separator from "./Separator";
 import Video from "./Video";
-import Date from "./Date"
+import Date from "./Date";
+import PriceFormat from "./PriceFormat";
 
-export { Button, Loading, Badge, Toggle, TouchOverlay, Separator, Video, Date };
+export {
+  Button,
+  Loading,
+  Badge,
+  Toggle,
+  TouchOverlay,
+  Separator,
+  Video,
+  Date,
+  PriceFormat,
+};
