d9e99b05a8e37b1c4a245eaded15bd9c948fac7e
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2023-10-26 10:52:44 -0300

    doc: update component Price Format

--- src/stories/PriceFormat.stories.ts
+++ src/stories/PriceFormat.stories.ts
@@ -15,8 +15,33 @@ export default {
 
 export const Default: StoryObj = {
   args: {
-    value: "130,00",
+    value: "120,05",
+    currency: "R$",
     color: "green",
     bold: true,
+    fontSize: 12,
+    strike: false,
+    decimalCut: false,
+  },
+};
+
+export const Disabled: StoryObj = {
+  args: {
+    value: "1.200,80",
+    currency: "R$",
+    fontSize: 15,
+    strike: true,
+    decimalCut: false,
+  },
+};
+
+export const Size: StoryObj = {
+  args: {
+    color: "red",
+    value: "30",
+    currency: "€",
+    fontSize: 28,
+    strike: false,
+    decimalCut: true,
   },
 };
